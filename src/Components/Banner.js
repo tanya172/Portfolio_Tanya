bbde1093da0e35bb3ab1e284223129f2f89bbb90
tanya172 <tannyaverma086@gmail.com>
2023-05-01 18:32:25 +0530

    bannerPage

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,7 @@
-import { Container, Row } from "react-bootstrap"
-
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
 export const Banner = () => {
     return(
         <section className="banner" id ="home">
@@ -10,7 +12,11 @@ export const Banner = () => {
                         <h1>{`Greeting, I'm Tanya`}
                         <span className="wrap">Student</span></h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        <button onClick={() => console.log('connect')}>Let's Connect</button>
+                        <button onClick={() => console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+
                     </Col>
                 </Row>
             </Container>
