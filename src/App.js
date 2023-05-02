cb508b686862d7701534217af958597a4d8a0d44
tanya172 <tannyaverma086@gmail.com>
2023-05-02 23:39:20 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './Components/Navbar';
 import {Banner} from './Components/Banner';
+import { Skills } from './Components/Skills';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
     <NavBar/>
     <Banner/>
+    <Skills/>
     </div>
   );
 }
