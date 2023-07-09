4e93ce77acb4ea821a489815ff49ba518005ff80
tanya172 <tannyaverma086@gmail.com>
2023-07-09 22:19:48 +0530

    Contact us page

---
+++ src/Components/Contact.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { Container } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+
+export const Contact = () =>{
+    const FromInitialDetails = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: ""
+    }
+    
+    const [formDetails,setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState("Send");
+    const [status, setStatus] = useState({});
+
+    return (
+          <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={contactImg} alt="Contact Me"/>
+                    </Col>
+                    <Col md ={6}>
+                    <h2>Let's Discuss</h2>
+
+                    </Col>
+                </Row>
+            </Container>
+          </section>
+    )
+}
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,8 +4,8 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+// import 'animate.css';
+// import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
@@ -47,7 +47,7 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
+            {/* <TrackVisibility> */}
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
@@ -88,7 +88,7 @@ export const Projects = () => {
                   </Tab.Content>
                 </Tab.Container>
               </div>}
-            </TrackVisibility>
+            {/* </TrackVisibility> */}
           </Col>
         </Row>
       </Container>
